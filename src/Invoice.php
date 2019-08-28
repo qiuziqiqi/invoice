410a908cddde5fd93b4e85941ab351c6de713339
simba.wu <simba.wu@dadaabc.com>
2019-08-28 16:10:17 +0800

    init project

---
+++ src/Invoice.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wuheng
+ * Date: 2019/8/27
+ * Time: 6:07 PM
+ */
+
+namespace Simba\Invoice;
+
+class Invoice
+{
+    public function make($config)
+    {
+
+    }
+}
